983361d108823bc658b911be36a0d67ce0d27abf
jonathan.pirro <jonathan.pirro@wbgames.com>
2017-12-28 10:09:35 -0800

    first main site setup

--- content/title-page.md
+++
@@ -1,5 +0,0 @@
----
-title: "THAT LOUD GUY | personal site of j. pirro"
-date: 2017-12-28T09:12:04-08:00
----
-
---
+++ public/js/bgimages.js
@@ -0,0 +1,48 @@
+String.prototype.hashCode = function(){
+    // djb2 hash algorithm
+    var hash = 5381;
+    for (i = 0; i < this.length; i++) {
+        char = this.charCodeAt(i);
+        hash = ((hash << 5) + hash) + char; /* hash * 33 + c */
+    }
+    return hash >>> 0;
+};
+
+var getBackgroundImages = function() {
+    var backgrounds = document.getElementsByTagName("body");
+    var images = [];
+    var imgString = backgrounds[0].getAttribute("data-backgrounds");
+    if (imgString) {
+        images = imgString.split(/[\s,]+/).filter(Boolean);
+    }
+    return images;
+};
+
+var preloadBackgrounds = function() {
+    var images = getBackgroundImages();
+    for(var i = 0; i < images.length; i++)
+    {
+        // caches images, avoiding white flash between background replacements
+        new Image().src = images[i];
+    }
+};
+
+var rotateBackground = function(count) {
+    if (count === undefined || count === null) {
+        count = 0;
+    }
+
+    var images = getBackgroundImages();
+    if (images.length > 0) {
+        count = (count+1) % images.length;
+        // console.log("rotating background to "+count);
+
+        document.body.style.background = 'url("' + images[count] +'")';
+        document.body.style.backgroundSize = "cover";
+        if (images.length > 1) {
+            setTimeout(rotateBackground.bind(null, count), 30000);
+        }
+    }
+};
+preloadBackgrounds();
+rotateBackground();
